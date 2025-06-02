1cc37f100e35e6f45c5d0b8e67b9ffa230e78ec0
Hužva <huzvanec@email.cz>
2025-06-02 18:46:25 +0200

    fix(build): fix version not injecting to paper-plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ tasks {
         val props = mapOf("version" to version)
         inputs.properties(props)
         filteringCharset = "UTF-8"
-        filesMatching("plugin.yml") {
+        filesMatching("paper-plugin.yml") {
             expand(props)
         }
     }
