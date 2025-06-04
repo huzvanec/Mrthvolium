ef13b7f1bf128773632d34bc1465f86bffdd5da3
Hužva <huzvanec@email.cz>
2025-06-04 22:57:44 +0200

    feat(core): add uninstall warning message on first start and disable the plugin

--- src/main/kotlin/cz/jeme/mrthvolium/Mrthvolium.kt
+++ src/main/kotlin/cz/jeme/mrthvolium/Mrthvolium.kt
@@ -3,14 +3,49 @@ package cz.jeme.mrthvolium
 import cz.jeme.mrthvolium.corpse.CorpseManager
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.key.KeyPattern
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 internal object Mrthvolium : JavaPlugin() {
     override fun onEnable() {
-        saveDefaultConfig()
-        
-        CorpseManager // initialize corpse manager
+        if (!File(dataFolder, "config.yml").exists()) {
+            // first start
+            componentLogger.warn(
+                Component.text(
+                    "This plugin is not designed to be safely uninstalled.",
+                    NamedTextColor.RED
+                ).decorate(TextDecoration.BOLD)
+            )
+            componentLogger.warn(
+                Component.text(
+                    "By using $name, you acknowledge that once a corpse has been created, the plugin cannot be removed without leaving behind permanent traces.",
+                    NamedTextColor.RED
+                )
+            )
+            componentLogger.info(
+                Component.text(
+                    "✅ If you wish to keep using this plugin, please restart the server now.",
+                    NamedTextColor.GREEN
+                )
+            )
+            componentLogger.info(
+                Component.text(
+                    "❌ If you do not wish to use it, stop the server and delete the plugin file from the plugins folder.",
+                    NamedTextColor.RED
+                )
+            )
+            componentLogger.info("$name will now be disabled until the next server restart.")
+            saveDefaultConfig()
+
+            Bukkit.getPluginManager().disablePlugin(this)
+        } else {
+            CorpseManager // initialize corpse manager
+        }
     }
 
     fun key(@KeyPattern.Value key: String): Key = NamespacedKey(this, key)
